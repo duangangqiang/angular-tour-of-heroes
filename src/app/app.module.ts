74b76b25bf291202772bd0e3efd062a50efb5fc1
gangqiang <gangqiang.duan@idsuipai.com>
2017-08-17 10:04:17 +0800

    http服务接入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -23,6 +26,7 @@ import { HeroService } from './hero.service';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     HttpModule
   ],
   providers: [HeroService],
